3cc404f5be54c15ff26c3293de0f2b5301cf92c6
xh <xuehao@wayyue.com>
2020-08-30 05:27:20 +0800

    Initial commit

---
+++ blog-user/src/test/java/com/waylau/spring/boot/blog/test.java
@@ -0,0 +1,12 @@
+package com.waylau.spring.boot.blog;
+
+/**
+ * @Author: XueHao
+ * @Date: 2020/08/30
+ * @Copyright: Copyright (c) wayyue.com Corporation. All Rights Reserved.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+}
